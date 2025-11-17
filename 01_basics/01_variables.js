c7dd3525bc966d8cc1c9c59f25af4da59bca8af3
Bhanu Pratap Singh <info2bhanusingh@gmail.com>
2025-11-17 11:19:03 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+ const accountId = 14453
+ let accountEmail  = "bhanu@google.com"
+ var accountPassword = "1234"
+ accountCity = "Noida"
+
+//  accountId = 2
+accountEmail = "bs&bs.com"
+accountPassword = "1253" 
+accountCity = "jaipur"
+
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountCity])
+
+ /*perfer not to use 'var' because of issue in block scope and functional scope */
--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,33 @@
 "use strict";
 
 console.table([2+2,3-3])
+
+// there are not many datatypes in js, it's simple and sorted
+
+let name = "bhanu"
+let age = 18
+let isLoggedIn = false
+
+
+
+//number => 2 to power 53
+//bigInt
+//string -> works with both single quote and double quote(prefer double quotes)
+// boolean =>true/false
+//null => standalone value (temperature example)
+// undefined =>
+//symbol => unique
+
+// these are the primitive data types
+
+//object
+
+console.log(typeof "bahl")
+console.log(typeof age)
+
+//loophole in js
+console.log(typeof null)
+// it returns object 
+console.log(typeof undefined)
+// it returns undefined
+
